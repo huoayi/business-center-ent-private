74548337dc648beb0307deac7357c7dcb55e60bf
huoayi <hyk130406@icloud.com>
2024-03-09 11:01:32 +0800

    feat: add main func to migrate

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"graduation-project-ent/common"
+	"graduation-project-ent/internal/db"
+	"graduation-project-ent/pkg/ent_work/user"
+)
+
+func main() {
+	ctx := context.Background()
+	users, err := db.DB.User.Query().Where(user.DeletedAt(common.ZeroTime)).First(ctx)
+	if err != nil {
+		logrus.Errorf("%+v", err)
+	}
+	fmt.Printf("%+v\n", users)
+}
